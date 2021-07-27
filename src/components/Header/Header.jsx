03d92d0979ffc47818d9d8ce181bc06483df4b30
jube <jubermomin323@gmail.com>
2021-07-27 22:55:48 +0530

    Character data shown on mouseover

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 export default function Header({characters, updateCharacter}) {
     const [searchText, setSearchText] = useState('');
--- src/components/ImageContainer/ImageContainer.jsx
+++ src/components/ImageContainer/ImageContainer.jsx
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css'
 
 export default function ImageContainer({characters}) {
-    console.log(characters)
+    // console.log(characters)
+    const [isData, setIsData] = useState(false)
+    const showData = (bool) =>{
+        if(bool){
+            console.log("mouseEnter")
+            setIsData(true)
+        }
+        else{
+            console.log("mouseLeave")
+            setIsData(false)
+        }
+        
+        // isData === false ? setIsData(true) : setIsData(false)
+    }
     return (
         <div className="d-flex flex-wrap">
             {
                 characters.map((item) =>{
                     return(
                         <div className="p-2" key={item.char_id}>
-                            <img src={item.img} alt={item.name} />
-                            <h2><figcaption className="text-center">{item.name}</figcaption></h2>
+                            {
+                                isData 
+                                ?
+                                (
+                                    <div className="flip-side" style={{width:"500px", height:"600px"}} onMouseLeave={()=>showData(false)}>
+                                        <h1>{item.name}</h1>
+                                        <hr />
+                                        <h3>Actor Name : {item.portrayed}</h3>
+                                        <h3>Nick Name : {item.nickname}</h3>
+                                        <h3>Birthday : {item.birthday}</h3>
+                                        <h3>Status : {item.status}</h3>
+                                    </div>
+                                )
+                                :
+                                (
+                                    <div className="image-side">
+                                        <img src={item.img} alt={item.name} onMouseEnter={()=>showData(true)} />
+                                    </div>
+                                )
+                            }
+                            {/* <h2><figcaption className="text-center">{item.name}</figcaption></h2> */}
                         </div>
                     )
                 })
